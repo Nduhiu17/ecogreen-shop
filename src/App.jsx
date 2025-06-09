4ead702c7dfa21bfc417ec8608b631619b6df34d
Antony Nduhiu <nduhiu254@gmail.com>
2025-06-09 04:21:39 +0300

    Add api key-

--- src/App.jsx
+++ src/App.jsx
@@ -822,7 +822,7 @@ function App() {
       let chatHistory = [];
       chatHistory.push({ role: "user", parts: [{ text: prompt }] });
       const payload = { contents: chatHistory };
-      const apiKey = "";
+      const apiKey = "AIzaSyC1q_KJysimHoEnfZgofyIrsahF7NLmP0c";
       const apiUrl = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`;
 
       const response = await fetch(apiUrl, {
